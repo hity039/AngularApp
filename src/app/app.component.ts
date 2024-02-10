6c8c4b3ed4033da5b898cc95a6aa226210e0a7fd
Hitesh <hitesh.yadav@vaaaninfra.com>
2024-02-10 10:32:16 +0530

    Intial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './Components/header/header.component';
+import { TopHeaderComponent } from './Components/top-header/top-header.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,HeaderComponent,TopHeaderComponent,FontAwesomeModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
