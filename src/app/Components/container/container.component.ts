0e92c9d29875f772eda42bf4fccda8144c3030b7
Hitesh <hitesh.yadav@vaaaninfra.com>
2024-02-15 17:54:45 +0530

    ng-content,ng-template,contentChildren,viewchildren completed

--- src/app/Components/container/container.component.ts
+++ src/app/Components/container/container.component.ts
@@ -3,11 +3,12 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { SearchComponent } from './search/search.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { FeaturedBrandsComponent } from './featured-brands/featured-brands.component';
 
 @Component({
   selector: 'app-container',
   standalone: true,
-  imports: [SearchComponent,ProductListComponent,ProductDetailComponent,CommonModule],
+  imports: [SearchComponent,ProductListComponent,ProductDetailComponent,CommonModule,FeaturedBrandsComponent],
   templateUrl: './container.component.html',
   styleUrl: './container.component.css',
   providers:[CurrencyPipe]
